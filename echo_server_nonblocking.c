7f36eb633ece0c07cc95d3722bd7c8bcdef712c0
Aven Bross <kieroda@gmail.com>
2022-08-20 02:38:36 -0500

    Removed queue logic from server since sends should never block.:

--- echo_server_nonblocking.c
+++ echo_server_nonblocking.c
@@ -4,14 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <unistd.h> // contains read/write sycalls
+#include <unistd.h> // contains close sycall
 
 #include <sys/socket.h> // contains socket, connect, accept syscalls
-
 #include <netinet/in.h> // needed for sockaddr_in and htonl functions
 #include <arpa/inet.h> // needed for inet_ntoa
 
-#include <poll.h> // needed for poll and struct pollfd
+#include <poll.h> // needed for poll syscall and struct pollfd
 
 #include <errno.h> // needed for errno global
 
@@ -22,84 +21,35 @@ enum {
   QUEUELEN = 8
 };
 
-struct message {
-    char buffer[MAXRECVLEN];
-    int length;
-};
-
-struct message_queue {
-    struct message data[QUEUELEN];
-    int first;
-    int last;
-};
-
-void pop_queue(struct message_queue* queue) {
-    memset(queue->data + queue->first, 0, sizeof(struct message));
-    queue->first = (queue->first + 1) % QUEUELEN;
-}
-
-void push_queue(struct message_queue* queue) {
-    queue->last = (queue->last + 1) % QUEUELEN;
-}
-
-struct message* front_queue(struct message_queue* queue) {
-    return queue->data + queue->first;
-}
-
-struct message* back_queue(struct message_queue* queue) {
-    return queue->data + queue->last;
-}
-
 struct pollfd connection_fds[MAXCONS+1];
-struct message_queue send_queue[MAXCONS];
-
-int send_message(int con_index) {
-    struct message* message;
-    int len = 0;
-    int confd = connection_fds[con_index].fd;
-    struct message_queue* queue = &(send_queue[con_index]);
-
-    while(queue->first != queue->last) {
-        message = front_queue(queue);
-        len = send(confd, message->buffer, message->length, 0);
-
-        if(len < 0) {
-            if(errno == EAGAIN || errno == EWOULDBLOCK) return 1;
-            return 0;
-        } if(len == 0) {
-            return 0;
-        }
-
-        printf("sent client %d: %s\n", confd, message->buffer);
-        pop_queue(queue);
-    }
-
-    return 1;
-}
 
 int handle_connection(int con_index) {
     int len = 0;
+    char buffer[MAXRECVLEN+1];
     int confd = connection_fds[con_index].fd;
-    struct message_queue* queue = &(send_queue[con_index]);
-    struct message* message = back_queue(queue);
 
-    len = recv(confd, message->buffer, MAXRECVLEN, 0);
-    
+    // read a new message from the socket
+    len = recv(confd, buffer, MAXRECVLEN, 0);
+   
     if(len < 0) {
+        // if the socket would block, quit without error
         if(errno == EAGAIN || errno == EWOULDBLOCK) return 1;
         return 0;
     } if(len == 0) {
         return 0;
     }
 
-    message->length = len;
-    message->buffer[len] = '\0';
+    buffer[len] = '\0';
+    printf("client %d sent: %s\n", con_index, buffer);
 
-    printf("client %d sent: %s\n", con_index, message->buffer);
+    // send the message back to the client
+    len = send(confd, buffer, len, 0);
 
-    push_queue(queue);
+    if(len <= 0) {
+        return 0;
+    }
 
-    return send_message(con_index);
+    return 1;
 }
 
 int accept_connection(int socketfd) {
@@ -122,8 +72,6 @@ int accept_connection(int socketfd) {
         &socksize,
         SOCK_NONBLOCK
     );
-
-    memset(&(send_queue[index]), 0, sizeof(struct message_queue));
 
     printf("Incoming connection from %s assigned to %d at index %d\n",
         inet_ntoa(dest.sin_addr), connection_fds[index].fd, index);
@@ -191,6 +139,7 @@ int main(int argc, char *argv[])
         // kill program if an error occurs (should never happen)
         if(num_events < 0) exit(1);
 
+        // handle any new events
         if(num_events > 0) {
             // accept any new connections
             if(connection_fds[0].revents !=0) {
@@ -203,21 +152,10 @@ int main(int argc, char *argv[])
                     if(handle_connection(i) < 1) {
                         clean_connection(i);
                     }
-                } else if((connection_fds[i].revents & POLLOUT) !=0) {
-                    if(send_message(i) < 1) {
-                        clean_connection(i);
-                    }
                 }
             }
         }
     }
     
-    for(int i = 1; i < MAXCONS+1; ++i) {
-        if(connection_fds[i].fd > 0) {
-            close(connection_fds[i].fd);
-        }
-    }
-
-    close(mysocket);
     return EXIT_SUCCESS;
 }
